8635b5166b3056e4b4574deb818021fa091b2ec5
AmidKanu <amidukannu@gmail.com>
2022-12-08 03:44:34 +0000

    first commit

---
+++ lesson5/js/javascript.js
@@ -0,0 +1,30 @@
+const input = document.querySelector('input');
+
+const button = document.querySelector('button');
+
+const list = document.querySelector('ul');
+
+
+button.addEventListener('click', function() {
+    const mychap = input.value;
+    input.value = ''; 
+
+    const listchap = document.createElement('li');
+
+    const listtext = document.createElement('span');
+
+    const listbutton = document.createElement('button');
+
+    listchap.appendChild(listtext);
+    listtext.textContent = mychap;
+    listchap.appendChild(listbutton);
+    listbutton.textContent = 'delete';
+    list.appendChild(listchap);
+
+    listbutton.addEventListener('click', function() {
+        list.removeChild(listchap);
+      });
+
+      input.focus();
+    });
+
